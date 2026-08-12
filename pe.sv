63f96624adc1dae3c0aeb98253283460e48a8a1f
Gianmarco Polizia <aipills@MBPdiGianmarco.wind3.hub>
2026-08-12 09:47:52 +0200

    V1-multiply+acc

--- pe.sv
+++ pe.sv
@@ -3,7 +3,10 @@ module pe #(
 )(
 	input logic signed [DATA_WIDTH-1:0] a,
 	input logic signed [DATA_WIDTH-1:0] b,
+	input logic signed [DATA_WIDTH-1:0] acc_in
 	output logic signed [(2*DATA_WIDTH)-1:0] y
 );
-	assign y = a*b;
+	always_comb begin
+		y = a*b + acc_in;
+	end
 endmodule
